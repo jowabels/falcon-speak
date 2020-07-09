694aea43bb37363760749f3cea2b1f37438bf589
jowabels <jowabels.sec@gmail.com>
2020-07-09 21:18:57 +0800

    added incident request and handling for cases with no detections/incidents

--- falcon-speak.py
+++ falcon-speak.py
@@ -12,19 +12,35 @@ import json
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-g", "--generate", action="store_true", help="generate oauth token. token is valid for 30 minutes")
-    parser.add_argument("-d", "--detections",action="store_true", help="retrieve Falcon detections. Returned data is < 10,000 items", default=False)
+    parser.add_argument("-d", "--detections", action="store_true", help="retrieve Falcon detections. Returned data is < 10,000 items", default=False)
+    parser.add_argument("-i", "--incidents", action="store_true", help="retrieve Falcon incidents. Returned data is < 500 items", default=False)
     args = parser.parse_args()
 
     if args.generate:
         print("\n[+] Requesting for oauth token...")
         get_token()
+        print("\n")
+
     elif args.detections:
         print("\n[+] Getting list of Falcon detections...")
-        detection_ids = get_detections_list()
+        detections_list = get_detections_list()
         print("\n[+] Getting full info on the detection items...")
-        get_detections_list_info(detection_ids)
+        get_detections_list_info(detections_list)
+        print("\n")
+
+    elif args.incidents:
+        print("\n[+] Getting list of Falcon incidents...")
+        incidents_list = get_incidents_list()
+        print("\n[+] Getting full info on the incident items...")
+        get_incidents_list_info(incidents_list)
+        print("\n")
+        
     else:
         parser.print_help(sys.stderr)
+
+
+
+
 
 
 def get_token():
@@ -45,14 +61,14 @@ def get_token():
 
     r = requests.post(endpoint_uri, data=data)
     if r.status_code == 201:
-        print("[+] Successful token request...")
+        print("\t-- Successful token request...")
         j = r.json()
         token = j["access_token"]
 
         with open(config.TOKEN_PATH, "w") as fo:
             fo.write(token)
-        print("[+] Successfully stored token to temporary file...")
-        print("[+] With a valid token, you may now use other script arguments...")
+        print("\t-- Successfully stored token to temporary file...")
+        print("\t-- With a valid token, you may now use other script arguments...")
     else:
         unsucessful_http_request(r)
 
@@ -67,7 +83,7 @@ def verify_token(offset=0, limit=1):
     
     with open(config.TOKEN_PATH, "r") as fo:
         token = fo.read()
-    print("[+] Verifying validity of current token...")
+    print("\t-- Verifying validity of current token...")
     
     endpoint_uri = "{}/detects/queries/detects/v1".format(config.API_URL)
     headers = {
@@ -82,10 +98,10 @@ def verify_token(offset=0, limit=1):
 
     r = requests.get(endpoint_uri, headers=headers, params=params)
     if r.status_code == 403:
-        print("[+] Current token already expired. Requesting new one...")
+        print("\t-- Current token already expired. Requesting new one...")
         get_token()
     elif r.status_code == 200:
-        print("[+] Current token is still valid. Proceeding to API requests...")
+        print("\t-- Current token is still valid. Proceeding to API requests...")
     else:
         unsucessful_http_request(r)
 
@@ -96,7 +112,7 @@ def read_token():
     '''
 
     if not os.path.exists(config.TOKEN_PATH):
-        print("[+] Temporary token file does not seem to exist. Exiting...")
+        print("\t-- Temporary token file does not seem to exist. Exiting...")
     else:
         with open(config.TOKEN_PATH, "r") as fo:
             token = fo.read()
@@ -128,16 +144,19 @@ def get_detections_list(offset=0, limit=10):
     # noting here that params=json.dumps(params) does NOT work, params should be a JSON object
     # this is difference to POST where data should be a string, not a JSON object
     if r.status_code == 200:
-        print("[+] Successful request for detection list...")
+        print("\t-- Successful request for detection list...")
         j = r.json()
-        print(json.dumps(j, indent=4))
+
+        if not j["resources"]:
+            print("\t-- Unfortunately detections list is empty. No detections! Exiting...")
+            sys.exit()
 
         return j["resources"]
     else:
         unsucessful_http_request(r)
 
 
-def get_detections_list_info(detection_ids):
+def get_detections_list_info(detections_list):
     '''
         from the list object of detection IDs obtained from get_detections_list, we now
         query a different API endpoint for more info on those detections
@@ -153,18 +172,82 @@ def get_detections_list_info(detection_ids):
         "Authorization" : "Bearer {}".format(token)
     }
     data = {
-        "ids" : detection_ids
+        "ids" : detections_list
     }
 
     r = requests.post(endpoint_uri, headers=headers, data=json.dumps(data))
     # noting here that data=data does NOT work, data should be a string NOT a JSON object
     # this is different to GET where params should be a JSON object
     if r.status_code == 200:
-        print("[+] Successful request for detection information...")
+        print("\t-- Successful request for detection information...")
         j = r.json()
         print(json.dumps(j, indent=4))
     else:
         unsucessful_http_request(r)
+
+
+def get_incidents_list(offset=0, limit=10):
+    '''
+        similar to get_detections_list, this returns a list object
+        of incident IDs. the list of IDs is in the "resources" key in
+        JSON response
+    '''
+
+    verify_token()
+    token = read_token()
+
+    endpoint_uri = "{}/incidents/queries/incidents/v1".format(config.API_URL)
+    headers = {
+        "Content-type" : "application/json",
+        "Accept" : "application/json",
+        "Authorization" : "Bearer {}".format(token)
+    }
+    params = {
+        "offset" : offset,
+        "limit" : limit
+    }
+
+    r = requests.get(endpoint_uri, headers=headers, params=params)
+    if r.status_code == 200:
+        print("\t-- Successful request for incident list...")
+        j = r.json()
+
+        if not j["resources"]:
+            print("\t-- Unfortunately incidents list is empty. No incidents! Exiting...")
+            sys.exit()
+        
+        return j["resources"]
+    else:
+        unsucessful_http_request(r)
+
+
+def get_incidents_list_info(incidents_list):
+    '''
+        from the list of incident IDs returned from get_incidents_list
+        we now query again for details about those incidents
+    '''
+
+    verify_token()
+    token = read_token()
+
+    endpoint_uri = "{}/incidents/entities/incidents/GET/v1".format(config.API_URL)
+    headers = {
+        "Content-type" : "application/json",
+        "Accept" : "application/json",
+        "Authorization" : "Bearer {}".format(token)
+    }
+    data = {
+        "ids" : incidents_list
+    }
+
+    r = requests.post(endpoint_uri, headers=headers, data=data)
+    if r.status_code == 200:
+        print("\t-- Successful request for incident information...")
+        j = r.json()
+        print(json.dumps(j, indent=4))
+    else:
+        unsucessful_http_request()
+
 
 
 def unsucessful_http_request(r):
@@ -173,12 +256,16 @@ def unsucessful_http_request(r):
         usually means the response code (r.status_code) of the request
         is not 200, 201 or 403 (403 is different since it is used for token verification)
     '''
-    print("[+] Unsuccessful request. Exiting...")
-    print("[+] Response code: {}...".format(r.status_code))
+
+    print("\n[+] Unsuccessful request. Exiting...")
+    print("\t-- Response code: {}...".format(r.status_code))
     j = r.json()
     errmsg = j["errors"][0]["message"]
-    print("[+] Error message: {}...".format(errmsg))
+    print("\t-- Error message: {}...".format(errmsg))
     sys.exit()
+
+
+
 
 
 if __name__ == "__main__":
